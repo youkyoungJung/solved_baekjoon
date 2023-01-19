22cb160dd6f0c71aca244f03b1d9096a6e15316e
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2023-01-19 21:28:14 +0900

    [Silver II] Title: 안녕, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.py
+++ 백준/Silver/1535. 안녕/안녕.py
@@ -1,16 +1,19 @@
 N = int(input())
-L = [int(x) for x in input().split()]
-J = [int(x) for x in input().split()]
+L = [int(x) for x in input().split()] #체력
+J = [int(x) for x in input().split()] #기쁨
 
-dp = [[0]*101 for _ in range(N+1)]
+dp = [[0]*101 for _ in range(N+1)] #행 - 한명의 사람, 열-체력
 
-for i in range(1, N+1):
-    s = L[i-1]
-    p = J[i-1]
+for i in range(1, N+1):# 한명의 사람을 순환
+    s = L[i-1]         # 해당 사람 만나는데 필요한 체력
+    p = J[i-1]         # 해당 사람 만나서 얻는 기쁨
     
-    for j in range(1, 101):
-        if j <= s:
-            dp[i][j] = dp[i-1][j]
+    for j in range(1, 101): #체력이 1-100일 경우
+        if j <= s:          #현재 남아 있는 체력으로 현재 사람을 만날 수 없는경우
+            dp[i][j] = dp[i-1][j] #현재 사람을 만나지 않음
+            
         else:
-            dp[i][j] = max(dp[i-1][j], p+dp[i-1][j-s])
+            dp[i][j] = max(dp[i-1][j], p+dp[i-1][j-s])#남은 체력으로 현재사람 만나는 경우 추가
+
+#사람을 안만나는 경우 vs 만나는 경우 중 더 큰 기쁨을 얻게되는 결과 선택
 print(dp[N][100])
