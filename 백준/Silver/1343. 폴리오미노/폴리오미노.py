0e58ebc476e6ab8956ac6d7b1dd7dece2c152cf0
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2023-01-25 23:16:05 +0900

    [Silver V] Title: 폴리오미노, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+board = input()
+board = board.replace('XXXX','AAAA')
+board = board.replace('XX', 'BB')
+
+if board.count('X') != 0:
+    print(-1)
+else:
+    print(board)
