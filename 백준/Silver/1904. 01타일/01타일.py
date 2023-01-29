56b397400a1f20f1852c41749da488ee3a297724
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2023-01-29 22:48:27 +0900

    [Silver III] Title: 01타일, Time: 316 ms, Memory: 68952 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -1,11 +1,9 @@
-import sys
-input = sys.stdin.readline
-
 n = int(input())
 dp = [0] * 1000001
 dp[1] = 1
 dp[2] = 2
 
-for k in range(3,n+1):
-    dp[k] = (dp[k-1]+ dp[k-2])%15746
+for i in range(3, n+1):
+    dp[i] = (dp[i-1]+dp[i-2])%15746
+
 print(dp[n])
