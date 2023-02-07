9d1af917d5d195f99af96102cb67098c55a8075f
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2023-02-07 19:24:08 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a = list(input())
+
+for i in range(n-1):
+    b = list(input())
+    for j in range(len(a)):
+        if a[j] != b[j]:
+            a[j] = '?'
+print(''.join(a))
