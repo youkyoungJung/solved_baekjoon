b769ad918f8fdd92efd6abf0ae5bee1abe4d2d54
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2023-02-23 10:28:10 +0900

    [Silver II] Title: 스택 수열, Time: 188 ms, Memory: 32840 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+stack = []
+answer = []
+cur = 1
+flag = 0
+#12345678
+#+++--
+#[4,3,6,8,7,5,2,1]
+
+for i in range(n):
+    num = int(input())
+    while cur <= num:
+        stack.append(cur)
+        answer.append('+')
+        cur += 1
+
+    if stack[-1] == num:
+        stack.pop()
+        answer.append('-')
+    else:
+        print('NO')
+        flag = 1
+        break
+if flag == 0:
+    for i in answer:
+        print(i)
