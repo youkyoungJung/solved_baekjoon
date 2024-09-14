37fd234476edf93d2f7e7cbe18cc9225c586acde
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-09-14 23:13:13 +0900

    [Silver I] Title: RGB거리, Time: 112 ms, Memory: 14532 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,33 +1,42 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[][] rgb = new int[3][n+1];
 		
-		// input
-		int N = Integer.parseInt(br.readLine());
-		int[][] colors = new int[N][3];
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			rgb[0][i] = Integer.parseInt(st.nextToken());
+			rgb[1][i] = Integer.parseInt(st.nextToken());
+			rgb[2][i] = Integer.parseInt(st.nextToken());
+		} //입력 끝
 		
-		for(int i=0; i<N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for(int j=0; j<3; j++) {
-				colors[i][j] = Integer.parseInt(st.nextToken());
-			}
+		int[][] dp = new int[3][n];
+		
+	    //dp 0행 채우기. rgb[i][0]값. - 초기화
+		for(int i = 0; i < 3; i++) {
+			dp[i][0] = rgb[i][0];
 		}
 		
-		// solution
-		int[][] dp = new int[N][3];
-		for(int i=0; i<3; i++) dp[0][i] = colors[0][i];
-		for(int i=1; i<N; i++) {
-			dp[i][0] = Math.min(dp[i-1][1] + colors[i][0], dp[i-1][2] + colors[i][0]);
-			dp[i][1] = Math.min(dp[i-1][0] + colors[i][1], dp[i-1][2] + colors[i][1]);
-			dp[i][2] = Math.min(dp[i-1][0] + colors[i][2], dp[i-1][1] + colors[i][2]);
+		int index = 0;
+		int min = Integer.MAX_VALUE;
+		for (int j = 1; j < n; j++) { //집 개수
+			
+			dp[0][j] = rgb[0][j] + Math.min(dp[1][j-1], dp[2][j-1]);
+			dp[1][j] = rgb[1][j] + Math.min(dp[0][j-1], dp[2][j-1]);
+			dp[2][j] = rgb[2][j] + Math.min(dp[0][j-1], dp[1][j-1]);
+			
 		}
-		
-		// output
-		System.out.println(Math.min(dp[N-1][0], Math.min(dp[N-1][1], dp[N-1][2])));
+			
+		System.out.println(Math.min(dp[0][n-1], Math.min(dp[1][n-1], dp[2][n-1])));
+					
 	}
 
 }
