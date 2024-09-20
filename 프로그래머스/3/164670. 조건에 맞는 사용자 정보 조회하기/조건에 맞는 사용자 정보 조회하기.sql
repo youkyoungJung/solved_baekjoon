f2615d3058dd2c280a353cd5d5beaedc8b448c33
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-09-20 16:58:04 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -0,0 +1,19 @@
+-- 중고 거래 게시물을 3건 이상 등록한 사용자 ID, 닉네임,전체주소,전화번호 조회
+-- 전체 주소는 시, 도로명, 상세주소 함께 출력
+-- 전화번호 XXX-XXXX-XXXX 하이픈 삽입
+-- 회원ID 내림차순
+
+WITH BOARD_THREE AS (
+    SELECT WRITER_ID
+    FROM USED_GOODS_BOARD
+    GROUP BY WRITER_ID
+    HAVING COUNT(*) >= 3
+)
+
+SELECT USER_ID, NICKNAME, CONCAT(CITY, ' ', STREET_ADDRESS1, ' ', STREET_ADDRESS2) AS 전체주소,
+CONCAT(LEFT(TLNO, 3), '-', MID(TLNO, 4, 4), '-', RIGHT(TLNO, 4)) AS 전화번호
+FROM USED_GOODS_USER
+WHERE USER_ID IN (SELECT * FROM BOARD_THREE)
+ORDER BY USER_ID DESC
+
+
