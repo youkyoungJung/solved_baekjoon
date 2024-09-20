33d51302c7a7ab451a1950bb2dfcb59f7d4e5986
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-09-20 15:06:36 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,17 @@
+-- 조회수가 가장 높은 중고거래 게시물
+-- 첨부파일 경로 조회
+-- FILE ID 내림차순, 
+-- 디렉터리/게시글ID/파일ID/파일이름/파일확장자
+
+
+WITH BEST_VIEWS AS (
+    SELECT BOARD_ID
+    FROM USED_GOODS_BOARD
+    ORDER BY VIEWS DESC
+    LIMIT 1
+)
+
+SELECT CONCAT('/home/grep/src/', BOARD_ID,'/' ,FILE_ID, FILE_NAME, FILE_EXT) AS FILE_PATH
+FROM USED_GOODS_FILE
+WHERE BOARD_ID IN (SELECT * FROM BEST_VIEWS)
+ORDER BY FILE_ID DESC
